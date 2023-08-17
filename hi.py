f6f6c0b2326566702d21aae9762f9715d35719c0
Abril Ureña <107883174+abrilurena@users.noreply.github.com>
2023-08-17 01:25:11 +0000

    leccion de copilot

---
+++ hi.py
@@ -0,0 +1,17 @@
+# write a lesson about python for begineers
+
+# 1. print
+print("Hello World!")
+
+# 2. variable
+
+# 3. input
+
+x = int(input("Please input a number: "))
+print(x)
+
+# 4. if
+if x > 10:
+    print("x is greater than 10")
+else:
+    print("x is less than 10")
