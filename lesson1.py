33e75adbdd0a661b9d40d0fcef95ed1bec267261
Abril Ureña <107883174+abrilurena@users.noreply.github.com>
2023-08-17 15:42:38 +0000

    changes

---
+++ lesson1.py
@@ -0,0 +1,33 @@
+# write a lesson about python for beginners
+
+# 1. What is Python?
+
+# Python is a programming language. It is used for many things, including 
+# web development, data science, and software development. It is a very
+# popular language, and is used by many companies, including Google,
+# Facebook, and Instagram.   
+# 
+
+# 2. Why should I learn Python?
+
+# Python is a very popular language, and is used by many companies, including
+# Google, Facebook, and Instagram. It is also a very easy language to learn,
+# and is a great first language to learn. It is also a very powerful language,
+# and can be used for many things, including web development, data science,
+# and software development.
+
+# example of a comment
+
+# 3. How do I write a comment in Python?
+
+# To write a comment in Python, you use the # symbol. Anything after the # symbol
+
+# exercise 1: print "Hello World" to the console
+
+print("Hello World")
+
+# exercise 2: print a string to the console
+
+print("This is a string")
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
